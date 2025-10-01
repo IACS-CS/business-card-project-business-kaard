f89831c8107976d8e9ffd5dc48b288e2d1f8020e
BlokMaster21309 <bmaster42070@gmail.com>
2025-10-01 11:00:49 -0400

    did a whole bunch of stuff but non if it box model :/

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// Add click event listeners to all cards
-document.querySelectorAll(".card").forEach((card) => {
+let play1 = true;
+let play2 = true;
+
+// Add click event listeners to card 1
+document.querySelectorAll(".card-1").forEach((card) => {
   card.addEventListener("click", () => {
     card.classList.toggle("flipped");
+    if(play1){
+      document.getElementById("appear-1").pause();
+      document.getElementById("appear-1").currentTime = 0;
+      document.getElementById("appear-1").play()
+      play1 = false
+    }
+    else {
+      play1 = true
+    }
   });
 });
+
+// Add click event listeners to card 2
+document.querySelectorAll(".card-2").forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+    if(play2){
+      document.getElementById("appear-2").pause();
+      document.getElementById("appear-2").currentTime = 0;
+      document.getElementById("appear-2").play()
+      play2 = false
+    }
+    else {
+      play2 = true
+    }
+  });
+});
+
